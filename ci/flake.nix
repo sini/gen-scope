2472dd640006f47ce6536806e15c5fcf1e62c992
Jason Bowman <jason@json64.dev>
2026-06-30 13:06:21 -0700

    chore: drop { } from prelude/select path-imports (bare-entry sweep)
    gen-prelude + gen-select entries are now bare values, not nullary fns, so
    `import "${gen-prelude}/lib" { }` drops its `{ }`. Bumps the gen-prelude
    (and, for gen-derive, gen-select) lock to the bare-entry revs.

--- ci/flake.nix
+++ ci/flake.nix
@@ -14,7 +14,7 @@
       ...
     }:
     let
-      prelude = import "${gen-prelude}/lib" { };
+      prelude = import "${gen-prelude}/lib";
       genScope = import ../lib { inherit prelude; };
     in
     gen.lib.mkCi {
